2e41371a5a5e734da40ccef215fe8734f5217182
hsyy04 <594659663@qq.com>
2021-06-10 17:58:39 +0800

    identify the name! ast almost done

--- src3.0/sysy_node.hpp
+++ src3.0/sysy_node.hpp
@@ -23,11 +23,11 @@ typedef enum{
     ConstDecl_,ConstDef_,ConstDefs_,ConstInitVal_,ConstInitVals_,
     UnaryExp_,UnaryOp_,PrimaryExp_,MulExp_,AddExp_,Exp_,Exps_,LVal_,
     IntConst_,ConstExp_,ConstExps_,FuncType_,FuncDef_,
-    InitVals_,InitVal_,VarDef_,VarDefs_,VarDecl_,
+    InitVals_,InitVal_,VarDefs_,VarDecl_,
     FuncRParams_,FuncRParamse_,FuncFParam_,FuncFParams_,
     LOrExp_,LAndExp_,EqExp_,RelExp_,Cond_,Stmt_,BlockItems_,
     BlockItem_,Block_,Stmt_Assign_,Stmt_Exp_,Stmt_If_,Stmt_IfElse_,Stmt_While_,Stmt_Return_,
-    MulExp_Mul_,MulExp_Div_,MulExp_Mod_,AddExp_Sub_,AddExp_Add_,RelExp_LT_,RelExp_BG_,RelExp_BQ_,RelExp_LQ_,EqExp_EQ_,EqExp_NEQ_,UnaryExp_func_,LVal_Array_
+    MulExp_Mul_,MulExp_Div_,MulExp_Mod_,AddExp_Sub_,AddExp_Add_,RelExp_LT_,RelExp_BG_,RelExp_BQ_,RelExp_LQ_,EqExp_EQ_,EqExp_NEQ_,UnaryExp_func_,LVal_Array_,VarDef_array_init_,VarDef_array_,VarDef_single_,VarDef_single_init_,FuncFParam_array_,FuncFParam_single_,FuncFParam_singleArray_,FuncDef_int_,FuncDef_void_,FuncDef_int_para_,FuncDef_void_para_,InitVal_EXP,InitVal_NULL,ConstDef_array_,ConstDef_single_
 } GrammaType;
 
 struct position_t{
