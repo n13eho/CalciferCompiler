9a6c6d4febad59a17af9dc3b7953fe64192d1121
user <1070507558@qq.com>
2021-06-22 19:35:41 +0800

    change BasicBlock

--- include/BasicBlock.h
+++ include/BasicBlock.h
@@ -1,13 +1,35 @@
+// #include <vector>
+
+// //基本块
+// class BasicBlock
+// {
+//     public:
+//     BasicBlock();
+
+//     protected:
+
+//     private:
+//     std::vector<int> InstrList;
+// };
+
 #include <vector>
 
 //基本块
 class BasicBlock
 {
-    public:
+private:
+    int _index;
+    std::string _label;
+
+public:
+    std::vector<BasicBlock *> succs;      //后继
+    std::vector<BasicBlock *> preds;      //前驱
+    std::vector<LinearIR *> instructions; //基本块包括的
+
     BasicBlock();
+    void AddInstruction(LinearIR *instruction);
+    static void AddLink(BasicBlock *pred, BasicBlock *succ);
+    BasicBlock(int index, std::string label) : _index(index), _label(label){};
 
-    protected:
-
-    private:
-    std::vector<int> InstrList;
+protected:
 };
