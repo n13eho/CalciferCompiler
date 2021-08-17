1acb27393c87702dcd19492bf0b6885506e4f11a
hsyy04 <594659663@qq.com>
2021-08-17 16:41:27 +0800

    取消一个看不懂的注释

--- src/main.cpp
+++ src/main.cpp
@@ -104,4 +104,4 @@ int main(int argc, char *argv[])
 }
 
 // ASan config
-// extern "C" [[maybe_unused]] const char *__asan_default_options() { return "alloc_dealloc_mismatch=0, detect_leaks=0"; }
+extern "C" [[maybe_unused]] const char *__asan_default_options() { return "alloc_dealloc_mismatch=0, detect_leaks=0"; }
